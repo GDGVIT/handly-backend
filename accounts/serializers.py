5281188d067ea871d9fa0950cb9de9b1a7aa72a7
Sai Sandeep Rayanuthala <43823311+raysandeep@users.noreply.github.com>
2020-05-30 20:53:39 +0530

    Removed un used fields

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import User
-from rest_framework.fields import CurrentUserDefault
 
 class UserSignupSerializer(serializers.ModelSerializer):
     class Meta:
